e4c0cefa33a7ed6e1b8006b88106b5b9c661daee
rhetthegreat <rhettdsouza13@gmail.com>
2016-08-23 03:06:56 +0530

    Updating Recent

--- plot.py
+++ plot.py
@@ -13,12 +13,7 @@ for i in xrange(len(a)):
     ydata.append(a[i][0])
     xdata.append(a[i][1])
 
-x_sm = np.array(xdata)
-y_sm = np.array(ydata)
 
-x_smooth = np.linspace(x_sm.min(), x_sm.max())
-y_smooth = spline(x, y, x_smooth)
-
-pl.plot(x_smooth,y_smooth)
+pl.scatter(xdata,ydata)
 pl.show()
 data.close()
