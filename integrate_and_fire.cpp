16c64702b1f4a458ffecd88a4c37b1ec2b9287e0
rhetthegreat <rhettdsouza13@gmail.com>
2016-08-24 02:08:16 +0530

    Three neuron system implement

--- integrate_and_fire.cpp
+++ integrate_and_fire.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<fstream>
+#include<vector>
 using namespace std;
-
+float VectCond[3][2];
 class Neuron{
 
   public:
@@ -17,48 +18,71 @@ class Neuron{
         this->Espike = Espike;
       }
       float calculateSlope(float V){
-          float slope;
-          slope = ((this->I + (this->gleak*this->Eleak))/this->gleak)-V;
+          float slope,constant;
+          constant = (this->I + (this->gleak*this->Eleak))/this->C;
+          for (int i=0; i<3; i++){
+            constant += ((VectCond[i][0])*VectCond[i][1])/this->C;
+        }
+          slope = constant;
+          for (int i=0; i<3; i++){
+            slope -= (VectCond[i][0]/this->C)*V;
+          }
           return slope;
       }
-      float runNeuron(){
 
-        float Vo,to,m,V1,t1;
-        Vo = Eleak;
-        to = 0;
-        fstream afile;
-        afile.open("VxT.txt", ios::out | ios::in );
-        for(int i =0; i<=200; i++){
-          m = calculateSlope(Vo);
-
-          V1 = Vo + m*0.01;
-          t1 = to + 0.01;
-
-          if (V1>=this->Ethresh){
-            V1 = this->Espike;
-            cout << "Voltage = " << V1 << " Time = " << t1 << endl;
-            afile << V1 << " " << t1 << endl;
-            Vo = this->Ek;
-            to = t1;
-
-            continue;
-          }
-          cout << "Voltage = " << V1 << " Time = " << t1 << endl;
-          afile << V1 << " " << t1 << endl;
-          Vo = V1;
-          to = t1;
-
-        }
-
-      }
 
 };
 
 int main()
 {
-  Neuron singleNeuron(0.1,0.3,0.1,0.3,0.5,0.1,1);
+  std::vector<Neuron> setNeuron;
 
-  singleNeuron.runNeuron();
+  setNeuron.push_back(Neuron(0.1,0.2,0.2,0.3,0.6,0.1,1));
+  setNeuron.push_back(Neuron(0.1,0.3,0.1,0.3,0.6,0.1,1));
+  setNeuron.push_back(Neuron(0.1,0.4,0.3,0.3,0.6,0.1,1));
+
+  VectCond[0][0] = 0.2;
+  VectCond[1][0] = 0.9;
+  VectCond[2][0] = 0.001;
+
+  float Vo[3],to[3],m[3],V1[3],t1[3];
+  Vo[0] = setNeuron[0].Eleak;
+  Vo[1] = setNeuron[1].Eleak;
+  Vo[2] = setNeuron[2].Eleak;
+
+  to[0] = 0;
+  to[1] = 0;
+  to[2] = 0;
+  fstream afile;
+  afile.open("VxT.txt", ios::out | ios::in );
+  for(int j =0; j<=100; j++){
+    m[0] = setNeuron[0].calculateSlope(Vo[0]);
+    m[1] = setNeuron[1].calculateSlope(Vo[1]);
+    m[2] = setNeuron[2].calculateSlope(Vo[2]);
+    for(int i=0; i<3; i++){
+    V1[i] = Vo[i] + m[i]*0.01;
+    t1[i] = to[i] + 0.01;
+
+    if (V1[i]>=setNeuron[i].Ethresh){
+      V1[i] = setNeuron[i].Espike;
+      cout << "Voltage = " << i << " "<< V1[i] << " Time = " << t1[i] << endl;
+      afile << V1[i] << " " << t1[i] << endl;
+      Vo[i] = setNeuron[i].Ek;
+      to[i] = t1[i];
+      VectCond[i][1]=Vo[i];
+
+    }
+    else{
+    cout << "Voltage = " << i << " " <<  V1[i] << " Time = " << t1[i] << endl;
+    afile << V1[i] << " " << t1[i] << endl;
+    Vo[i] = V1[i];
+    to[i] = t1[i];
+    VectCond[i][1]=Vo[i];
+    }
+    }
+  }
+
+
 
   return 0;
 }
--- plot.py
+++ plot.py
@@ -1,19 +1,37 @@
 import matplotlib.pyplot as pl
 import numpy as np
-a=[]
+first=[]
+second=[]
+third=[]
 data = open("VxT.txt", "r+")
 
 listOfData = data.readlines()
+i=0
+while(i<len(listOfData)):
+    first.append(listOfData[i].split())
+    second.append(listOfData[i+1].split())
+    third.append(listOfData[i+2].split())
+    i+=3
+x1data = []
+y1data = []
+x2data = []
+y2data = []
+x3data = []
+y3data = []
+for j in xrange(len(first)):
+    y1data.append(first[j][0])
+    x1data.append(first[j][1])
+for j in xrange(len(second)):
+    y2data.append(second[j][0])
+    x2data.append(second[j][1])
+for j in xrange(len(third)):
+    y3data.append(third[j][0])
+    x3data.append(third[j][1])
 
-for i in xrange(len(listOfData)):
-    a.append(listOfData[i].split())
-xdata = []
-ydata = []
-for i in xrange(len(a)):
-    ydata.append(a[i][0])
-    xdata.append(a[i][1])
 
-
-pl.scatter(xdata,ydata)
+line1, = pl.plot(x1data,y1data, label = "First")
+line2, = pl.plot(x2data,y2data, label= "Second")
+line3, = pl.plot(x3data,y3data, label = "Third")
+pl.legend(handles = [line1,line2,line3])
 pl.show()
 data.close()
